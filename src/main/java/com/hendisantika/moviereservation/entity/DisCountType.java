349c8cdf3b4af6ab4f0d3e1600c5de8f720d2e9d
Hendi Santika <hendisantika@gmail.com>
2018-07-10 06:46:53 +0700

    #4 - Add DisCount Type

---
+++ src/main/java/com/hendisantika/moviereservation/entity/DisCountType.java
@@ -0,0 +1,16 @@
+package com.hendisantika.moviereservation.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : movie-reservation
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/07/18
+ * Time: 06.46
+ * To change this template use File | Settings | File Templates.
+ */
+public enum DisCountType {
+    AMOUNT,
+    PERCENT
+}
