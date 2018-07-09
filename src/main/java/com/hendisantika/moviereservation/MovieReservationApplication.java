4690ee0600528e3ec56f241346f1082b49475bb8
Hendi Santika <hendisantika@gmail.com>
2018-07-10 06:41:01 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/moviereservation/MovieReservationApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.moviereservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieReservationApplication.class, args);
+    }
+}
