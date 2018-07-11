6e8956208782e1c43921d095411e95a910db48f1
Hendi Santika <hendisantika@gmail.com>
2018-07-11 07:28:06 +0700

    #13 - Add DisCount Strategy

---
+++ src/main/java/com/hendisantika/moviereservation/strategy/DisCountStrategy.java
@@ -0,0 +1,37 @@
+package com.hendisantika.moviereservation.strategy;
+
+import com.hendisantika.moviereservation.entity.Screen;
+import com.hendisantika.moviereservation.rule.Rule;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : movie-reservation
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/07/18
+ * Time: 07.27
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class DisCountStrategy {
+
+    private final List<Rule> rules;
+
+    protected DisCountStrategy(List<Rule> rules) {
+        this.rules = rules;
+    }
+
+    public BigDecimal calculateDisCountFee(Screen screen) {
+        for (Rule rule : rules) {
+            if (rule.isStatisfiedBy(screen)) {
+                return getDiscountFee(screen);
+            }
+        }
+        return BigDecimal.ZERO;
+    }
+
+    protected abstract BigDecimal getDiscountFee(Screen screen);
+}
