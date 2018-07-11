9a810f4c917ec8202b848d1bf7de6561210af054
Hendi Santika <hendisantika@gmail.com>
2018-07-12 05:13:55 +0700

    #23 - Add Screen Repository

---
+++ src/main/java/com/hendisantika/moviereservation/repository/ScreenRepository.java
@@ -0,0 +1,17 @@
+package com.hendisantika.moviereservation.repository;
+
+import com.hendisantika.moviereservation.entity.Screen;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : movie-reservation
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/07/18
+ * Time: 05.13
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ScreenRepository extends JpaRepository<Screen, Long> {
+}
