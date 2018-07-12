7ec49921301ba004b22f2e1f6150ca2713893188
Hendi Santika <hendisantika@gmail.com>
2018-07-13 06:36:51 +0700

    #33 - Add Customer Contoller

---
+++ src/main/java/com/hendisantika/moviereservation/controller/CustomerController.java
@@ -0,0 +1,38 @@
+package com.hendisantika.moviereservation.controller;
+
+import com.hendisantika.moviereservation.dto.CustomerDto;
+import com.hendisantika.moviereservation.entity.Customer;
+import com.hendisantika.moviereservation.repository.CustomerRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : movie-reservation
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 13/07/18
+ * Time: 06.33
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @GetMapping("/{id}")
+    public CustomerDto.Customer getCusomter(@PathVariable Long id) {
+        Customer customer = customerRepository.findOne(id);
+        return modelMapper.map(customer, CustomerDto.Customer.class);
+    }
+}
